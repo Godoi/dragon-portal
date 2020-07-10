0614ff7f7e14f5a15c9c53b37886a068c2461558
Anderson Godoi Bernardes <andersongodoipoa@gmail.com>
2020-07-10 06:22:00 -0300

    Adding a test.

--- cypress/integration/application/detail.spec.js
+++ cypress/integration/application/detail.spec.js
@@ -1,5 +1,6 @@
 context('Application Dragon Portal', () => { 
   describe('Accessing Detail dragon', function() { 
+    let id = {};
     beforeEach(function () {
       cy.visit('http://localhost:4200/');
       cy.get('[data-cy=username]').type('demo');
@@ -7,16 +8,16 @@ context('Application Dragon Portal', () => {
       cy.wait(1000);
       cy.get('[data-cy=cySubmit]').click();  
       cy.wait(2000);
+      cy.get('[data-cy=id]').eq(0).should(($identifier) => {
+        id = $identifier[0].innerText;
+      });
     });
     it('successfully loads detail', function() { 
-      cy.get('[data-cy=id]').eq(0).should(($identifier) => {
-        const id = $identifier[0].innerText;
-         cy.get('[data-cy=btn-view]').eq(0).click(); 
-          cy.url().should('include', `/detail/${id}`)
-          cy.wait(1000);
-          cy.location().should(loc => {
-            expect(loc.href).to.eq(`http://localhost:4200/detail/${id}`);
-          });
+      cy.get('[data-cy=btn-view]').eq(0).click(); 
+      cy.url().should('include', `/detail/${id}`);
+      cy.wait(1000);
+      cy.location().should(loc => {
+        expect(loc.href).to.eq(`http://localhost:4200/detail/${id}`);
       });
     });
   });
